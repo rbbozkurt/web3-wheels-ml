0aa657f65ae3bbcc9f57ecf30f619d5c6094586d
rbbozkurt <resit.b.bozkuXrt@campus.tu-berlin.de>
2024-05-01 14:09:07 +0200

    main function added
    Signed-off-by: rbbozkurt <resit.b.bozkuXrt@campus.tu-berlin.de>

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 # SPDX-License-Identifier: MIT
 # SPDX-FileCopyrightText: 2023 R. Berkay Bozkurt <resitberkaybozkurt@gmail.com>
 
-
-if __name__ == "__main__":
+def main():
   ## start of main ##
   return 0
+
+if __name__ == "__main__":
+  main()
