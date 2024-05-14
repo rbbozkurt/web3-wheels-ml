e6eb31e840eb57d05d2631a97314145089f4e14d
HarshilDave1 <harshil128@gmail.com>
2024-05-13 23:20:35 -0400

    organize files

--- src/demos/qtable_taxiv3/main.py
+++ src/demos/qtable_taxiv3/main.py
@@ -1,19 +1,14 @@
 # SPDX-License-Identifier: MIT
 # SPDX-FileCopyrightText: 2023 R. Berkay Bozkurt <resitberkaybozkurt@gmail.com>
+import random
+
+import numpy as np
 from pyvirtualdisplay import Display
 
 from demos import Action_To_String, GridWorldEnv, grid_world_5_x_5
 
 virtual_display = Display(visible=0, size=(1400, 900))
 virtual_display.start()
-
-import os
-import random
-
-import gymnasium as gym
-import imageio
-import numpy as np
-import pickle5 as pickle
 
 
 def print_actions(actions: list):
@@ -142,155 +137,5 @@ def main():
     grid_world.close()
 
 
-def main2():
-    env = GridWorldEnv(
-        map_string=grid_world_5_x_5, n_vehicle=1, n_passenger=1, n_gas_station=1
-    )
-
-    state_space = 5 * 5 * 4 * 25
-    action_space = 6
-
-    Qtable_taxi = initialize_q_table(state_space, action_space)
-
-    # Training parameters
-    n_training_episodes = 100000  # Total training episodes
-
-    # Evaluation parameters
-    n_eval_episodes = 99  # Total number of test episodes
-
-    # DO NOT MODIFY EVAL_SEED
-    eval_seed = [
-        16,
-        54,
-        165,
-        177,
-        191,
-        191,
-        120,
-        80,
-        149,
-        178,
-        48,
-        38,
-        6,
-        125,
-        174,
-        73,
-        50,
-        172,
-        100,
-        148,
-        146,
-        6,
-        25,
-        40,
-        68,
-        148,
-        49,
-        167,
-        9,
-        97,
-        164,
-        176,
-        61,
-        7,
-        54,
-        55,
-        161,
-        131,
-        184,
-        51,
-        170,
-        12,
-        120,
-        113,
-        95,
-        126,
-        51,
-        98,
-        36,
-        135,
-        54,
-        82,
-        45,
-        95,
-        89,
-        59,
-        95,
-        124,
-        9,
-        113,
-        58,
-        85,
-        51,
-        134,
-        121,
-        169,
-        105,
-        21,
-        30,
-        11,
-        50,
-        65,
-        12,
-        43,
-        82,
-        145,
-        152,
-        97,
-        106,
-        55,
-        31,
-        85,
-        38,
-        112,
-        102,
-        168,
-        123,
-        97,
-        21,
-        83,
-        158,
-        26,
-        80,
-        63,
-        5,
-        81,
-        32,
-        11,
-        28,
-        148,
-    ]  # Evaluation seed, this ensures that all classmates agents are trained on the same taxi starting position
-    # Each seed has a specific starting state
-
-    # Environment parameters
-    env_id = "Taxi-v3"  # Name of the environment
-    max_steps = 200  # Max steps per episode
-
-    # Exploration parameters
-    max_epsilon = 1.0  # Exploration probability at start
-    min_epsilon = 0.05  # Minimum exploration probability
-    decay_rate = 0.005  # Exponential decay rate for exploration prob
-
-    Qtable_taxi = train(
-        n_training_episodes,
-        min_epsilon,
-        max_epsilon,
-        decay_rate,
-        env,
-        max_steps,
-        Qtable_taxi,
-    )
-
-    # Define the filename to save the Q-table
-    q_table_file = "src/demos/qtable_taxiv3/q_table_taxi.pkl"
-
-    # Save the Q-table to disk
-    with open(q_table_file, "wb") as f:
-        pickle.dump(Qtable_taxi, f)
-
-    print("Q-table saved to:", q_table_file)
-
-
 if __name__ == "__main__":
-    main2()
+    main()
---
+++ src/models/networks.py
@@ -0,0 +1,2 @@
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2024 Harshil Dave <harshil128@gmail.com>
